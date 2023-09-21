e77936202be0999670913d94360c284a8b254169
Egor Lappo <elappo@stanford.edu>
2023-09-21 09:46:23 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Structure v2.3.4";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        structure-drv = pkgs.stdenv.mkDerivation
+          {
+            name = "structure";
+            version = "2.3.4";
+            src = pkgs.fetchzip {
+              url = "https://web.stanford.edu/group/pritchardlab/structure_software/release_versions/v2.3.4/structure_kernel_source.tar.gz";
+              hash = "sha256-00MnGDhK+n0Eut2brL9w1gfZ1fdXUpmVPAZHlVew6lA=";
+            };
+
+            buildPhase = ''
+              make 
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp -r structure $out/bin
+            '';
+
+          };
+      in
+      rec
+      {
+
+        packages.default = structure-drv;
+
+      });
+}
