d555a20c6028209c5090f72e9b602b01ced87eac
Egor Lappo <elappo@stanford.edu>
2023-09-21 09:50:37 -0700

    working flake

--- flake.nix
+++ flake.nix
@@ -20,8 +20,10 @@
             version = "2.3.4";
             src = pkgs.fetchzip {
               url = "https://web.stanford.edu/group/pritchardlab/structure_software/release_versions/v2.3.4/structure_kernel_source.tar.gz";
-              hash = "sha256-00MnGDhK+n0Eut2brL9w1gfZ1fdXUpmVPAZHlVew6lA=";
+              hash = "sha256-IR/90aUON68dSrJAAOzEFB1xsIRe28k0EuaMVxFSABg=";
             };
+
+            NIX_CFLAGS_COMPILE = "-fcommon";
 
             buildPhase = ''
               make
@@ -29,7 +31,7 @@
 
             installPhase = ''
               mkdir -p $out/bin
-              cp -r ../exe/* $out/bin
+              cp -r structure $out/bin
             '';
 
           };
